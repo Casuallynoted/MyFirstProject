805c2749097cde4ed5d9cb62c91bab94f945b910
Ryan Thomas <teacher@MacBook-Pro.local>
2014-09-21 21:22:46 -0400

    First Rev of Project

--- MyFirstApplication/ViewController.swift
+++ MyFirstApplication/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var EnterPass: UITextField!
+    @IBOutlet weak var EnterUsername: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+ 
+    @IBOutlet weak var messageLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func LoginButtonPress(sender: UIButton) {
+        //Code will evaluate when we press the button.
+        messageLabel.hidden = false
+        messageLabel.textColor = UIColor.blueColor()
+        messageLabel.text = EnterUsername.text + " has been signed in!"
+        EnterPass.resignFirstResponder()
+    loginButton.setTitle("Logged In", forState: UIControlState.Normal)}
 }
 
